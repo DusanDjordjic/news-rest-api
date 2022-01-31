55a1d47bfc58d01a4b72176baa010e266d57f17b
Dusan Djordjic <dusan.djordjic@mbet.com>
2022-01-31 10:17:20 +0100

    [na-01] Did some minor changes

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const newsRoutes = require("./routes/news");
 const errorController = require("./middleware/error");
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use("/news", newsRoutes.router);
 app.use(errorController.errorHandler);
 // Starnig server
